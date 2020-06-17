c5aeef88bdc11f7ceaf6ebf418ba512371a8a915
vcjmhg <13069648310@163.com>
2020-06-17 19:08:45 +0800

    [char 2]完成简单的列表展示，当时当前遇到控制器不能被识别的问题

---
+++ src/main/java/pojo/Taco.java
@@ -0,0 +1,41 @@
+package pojo;
+
+import java.util.List;
+
+public class Taco {
+    private String name;
+    private List<String> ingredients;
+
+    public Taco() {
+
+    }
+
+    @Override
+    public String toString() {
+        return "Taco{" +
+                "name='" + name + '\'' +
+                ", ingredients=" + ingredients +
+                '}';
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public List<String> getIngredients() {
+        return ingredients;
+    }
+
+    public void setIngredients(List<String> ingredients) {
+        this.ingredients = ingredients;
+    }
+
+    public Taco(String name, List<String> ingredients) {
+        this.name = name;
+        this.ingredients = ingredients;
+    }
+}
